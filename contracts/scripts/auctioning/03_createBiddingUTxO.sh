0f0ffbc287c19f69ce3c26d533620b11ea86c881
logicalmechanism <m.quinn.p@protonmail.com>
2023-01-07 13:29:17 -0800

    initial auction test is good

--- contracts/scripts/auctioning/03_createBiddingUTxO.sh
+++ contracts/scripts/auctioning/03_createBiddingUTxO.sh
@@ -10,6 +10,10 @@ script_path="../../swap-contract/swap-contract.plutus"
 stake_path="../../stake-contract/stake-contract.plutus"
 script_address=$(${cli} address build --payment-script-file ${script_path} --stake-script-file ${stake_path} --testnet-magic ${testnet_magic})
 
+# seller
+seller_address=$(cat ../wallets/seller-wallet/payment.addr)
+seller_pkh=$(${cli} address key-hash --payment-verification-key-file ../wallets/seller-wallet/payment.vkey)
+
 # buyer 
 buyer_address=$(cat ../wallets/buyer-wallet/payment.addr)
 buyer_pkh=$(${cli} address key-hash --payment-verification-key-file ../wallets/buyer-wallet/payment.vkey)
@@ -42,6 +46,37 @@ fi
 alltxin=""
 TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' ../tmp/buyer_utxo.json)
 buyer_tx_in=${TXIN::-8}
+
+# get script utxo data
+echo -e "\033[0;36m Gathering Script UTxO Information  \033[0m"
+${cli} query utxo \
+    --address ${script_address} \
+    --testnet-magic ${testnet_magic} \
+    --out-file ../tmp/script_utxo.json
+TXNS=$(jq length ../tmp/script_utxo.json)
+if [ "${TXNS}" -eq "0" ]; then
+   echo -e "\n \033[0;31m NO UTxOs Found At ${script_address} \033[0m \n";
+   exit;
+fi
+alltxin=""
+TXIN=$(jq -r --arg alltxin "" --arg sellerPkh "${seller_pkh}" 'to_entries[] | select(.value.inlineDatum.fields[0].fields[0].bytes == $sellerPkh) | .key | . + $alltxin + " --tx-in"' ../tmp/script_utxo.json)
+sale_tx_in=${TXIN::-8}
+string=${sale_tx_in}
+IFS='#' read -ra array <<< "$string"
+# update tx id info
+variable=${array[0]}; jq --arg variable "$variable" '.fields[1].fields[0].bytes=$variable' ../data/bidding/bidding-datum.json > ../data/bidding/bidding-datum-new.json
+mv ../data/bidding/bidding-datum-new.json ../data/bidding/bidding-datum.json
+variable=${array[1]}; jq --argjson variable "$variable" '.fields[1].fields[1].int=$variable' ../data/bidding/bidding-datum.json > ../data/bidding/bidding-datum-new.json
+mv ../data/bidding/bidding-datum-new.json ../data/bidding/bidding-datum.json
+
+# update the min utxo with new datum
+min_utxo=$(${cli} transaction calculate-min-required-utxo \
+    --babbage-era \
+    --protocol-params-file ../tmp/protocol.json \
+    --tx-out-inline-datum-file ../data/bidding/bidding-datum.json \
+    --tx-out="${script_address} + 5000000 + ${asset}" | tr -dc '0-9')
+
+script_address_out="${script_address} + ${min_utxo} + ${asset}"
 
 echo -e "\033[0;36m Building Bid \033[0m"
 FEE=$(${cli} transaction build \
--- contracts/scripts/auctioning/06_completeAuctionableUTxO.sh
+++ contracts/scripts/auctioning/06_completeAuctionableUTxO.sh
@@ -5,47 +5,82 @@ export CARDANO_NODE_SOCKET_PATH=$(cat path_to_socket.sh)
 cli=$(cat path_to_cli.sh)
 testnet_magic=$(cat ../data/testnet.magic)
 
-#
+# staked smart contract address
 script_path="../../swap-contract/swap-contract.plutus"
-script_address=$(${cli} address build --payment-script-file ${script_path} --testnet-magic ${testnet_magic})
-#
-seller_address=$(cat ../wallets/seller-wallet/payment.addr)
+stake_path="../../stake-contract/stake-contract.plutus"
+script_address=$(${cli} address build --payment-script-file ${script_path} --stake-script-file ${stake_path} --testnet-magic ${testnet_magic})
 
+# collat
+collat_address=$(cat ../wallets/collat-wallet/payment.addr)
+collat_pkh=$(${cli} address key-hash --payment-verification-key-file ../wallets/collat-wallet/payment.vkey)
+
+# seller
+seller_address=$(cat ../wallets/seller-wallet/payment.addr)
+seller_pkh=$(${cli} address key-hash --payment-verification-key-file ../wallets/seller-wallet/payment.vkey)
+
+# buyer
 buyer_address=$(cat ../wallets/buyer-wallet/payment.addr)
 buyer_pkh=$(${cli} address key-hash --payment-verification-key-file ../wallets/buyer-wallet/payment.vkey)
-#
-asset="1 f61e1c1d38fc4e5b0734329a4b7b820b76bb8e0729458c153c4248ea.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6731"
-min_asset="5000000 + ${asset}"
 
-min_utxo=$(${cli} transaction calculate-min-required-utxo \
+#
+auction_asset="1 29554843ec2823b1a3b1bf1abd21b1bb0862d5efa6dea0838c9da0ee.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6730"
+
+bid_asset="12345 f61e1c1d38fc4e5b0734329a4b7b820b76bb8e0729458c153c4248ea.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6739"
+
+bid_min_utxo=$(${cli} transaction calculate-min-required-utxo \
+    --babbage-era \
+    --protocol-params-file ../tmp/protocol.json \
+    --tx-out-inline-datum-file ../data/bidding/bidding-datum.json \
+    --tx-out="${script_address} + 5000000 + ${bid_asset}" | tr -dc '0-9')
+
+current_min_utxo=$(${cli} transaction calculate-min-required-utxo \
     --babbage-era \
     --protocol-params-file ../tmp/protocol.json \
     --tx-out-inline-datum-file ../data/auctionable/auctionable-datum.json \
-    --tx-out="${script_address} ${min_asset}" | tr -dc '0-9')
-min_utxo=2594620 # worst case for auction datum
+    --tx-out="${script_address} + 5000000 + ${auction_asset}" | tr -dc '0-9')
 
-sc_address_out="${script_address} + ${min_utxo} + ${asset}"
-seller_address_out="${seller_address} + 10000000"
-echo "Exit OUTPUT: "${seller_address_out}
+next_min_utxo=$(${cli} transaction calculate-min-required-utxo \
+    --babbage-era \
+    --protocol-params-file ../tmp/protocol.json \
+    --tx-out-inline-datum-file ../data/swappable/buyer-swappable-datum.json \
+    --tx-out="${script_address} + 5000000 + ${auction_asset}" | tr -dc '0-9')
+
+difference=$((${next_min_utxo} - ${current_min_utxo}))
+
+if [ "$difference" -lt "0" ]; then
+    min_utxo=${current_min_utxo}
+    # update the increase ada in the redeemer
+    variable=0; jq --argjson variable "$variable" '.fields[0].fields[0].int=$variable' ../data/redeemers/offer-redeemer.json > ../data/redeemers/offer-redeemer-new.json
+    mv ../data/redeemers/offer-redeemer-new.json ../data/redeemers/offer-redeemer.json
+else
+    echo "Increase Min ADA by" ${difference}
+    min_utxo=${next_min_utxo}
+    # update the increase ada in the redeemer
+    variable=${difference}; jq --argjson variable "$variable" '.fields[0].fields[0].int=$variable' ../data/redeemers/offer-redeemer.json > ../data/redeemers/offer-redeemer-new.json
+    mv ../data/redeemers/offer-redeemer-new.json ../data/redeemers/offer-redeemer.json
+fi
+
+script_address_out="${script_address} + ${min_utxo} + ${auction_asset}"
+seller_address_out="${seller_address} + ${bid_min_utxo} + ${bid_asset}"
+echo "Script OUTPUT: "${script_address_out}
+echo "Payment OUTPUT: "${seller_address_out}
 #
 # exit
 #
 echo -e "\033[0;36m Gathering UTxO Information  \033[0m"
 ${cli} query utxo \
     --testnet-magic ${testnet_magic} \
-    --address ${buyer_address} \
-    --out-file ../tmp/buyer_utxo.json
-
-TXNS=$(jq length ../tmp/buyer_utxo.json)
+    --address ${seller_address} \
+    --out-file ../tmp/seller_utxo.json
+TXNS=$(jq length ../tmp/seller_utxo.json)
 if [ "${TXNS}" -eq "0" ]; then
-   echo -e "\n \033[0;31m NO UTxOs Found At ${buyer_address} \033[0m \n";
+   echo -e "\n \033[0;31m NO UTxOs Found At ${seller_address} \033[0m \n";
    exit;
 fi
 alltxin=""
-TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' ../tmp/buyer_utxo.json)
-CTXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in-collateral"' ../tmp/buyer_utxo.json)
-collateral_tx_in=${CTXIN::-19}
-buyer_tx_in=${TXIN::-8}
+TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' ../tmp/seller_utxo.json)
+seller_tx_in=${TXIN::-8}
+
 
 echo -e "\033[0;36m Gathering Script UTxO Information  \033[0m"
 ${cli} query utxo \
@@ -59,37 +94,65 @@ if [ "${TXNS}" -eq "0" ]; then
    exit;
 fi
 alltxin=""
-TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' ../tmp/script_utxo.json)
-script_tx_in=${TXIN::-8}
+TXIN=$(jq -r --arg alltxin "" --arg sellerPkh "${seller_pkh}" 'to_entries[] | select(.value.inlineDatum.fields[0].fields[0].bytes == $sellerPkh) | .key | . + $alltxin + " --tx-in"' ../tmp/script_utxo.json)
+auction_tx_in=${TXIN::-8}
 
-script_ref_utxo=$(${cli} transaction txid --tx-file ../tmp/swap-reference-utxo.signed )
+TXIN=$(jq -r --arg alltxin "" --arg buyerPkh "${buyer_pkh}" 'to_entries[] | select(.value.inlineDatum.fields[0].fields[0].bytes == $buyerPkh) | .key | . + $alltxin + " --tx-in"' ../tmp/script_utxo.json)
+bid_tx_in=${TXIN::-8}
+
+string=${bid_tx_in}
+IFS='#' read -ra array <<< "$string"
+# update tx id info
+variable=${array[0]}; jq --arg variable "$variable" '.fields[1].fields[0].bytes=$variable' ../data/redeemers/offer-redeemer.json > ../data/redeemers/offer-redeemer-new.json
+mv ../data/redeemers/offer-redeemer-new.json ../data/redeemers/offer-redeemer.json
+variable=${array[1]}; jq --argjson variable "$variable" '.fields[1].fields[1].int=$variable' ../data/redeemers/offer-redeemer.json > ../data/redeemers/offer-redeemer-new.json
+mv ../data/redeemers/offer-redeemer-new.json ../data/redeemers/offer-redeemer.json
+
 # collat info
-collat_pkh=$(${cli} address key-hash --payment-verification-key-file ../wallets/collat-wallet/payment.vkey)
-collat_utxo="10e5b05d90199da3f7cb581f00926f5003e22aac8a3d5a33607cd4c57d13aaf3" # in collat wallet
+echo -e "\033[0;36m Gathering Collateral UTxO Information  \033[0m"
+${cli} query utxo \
+    --testnet-magic ${testnet_magic} \
+    --address ${collat_address} \
+    --out-file ../tmp/collat_utxo.json
+TXNS=$(jq length ../tmp/collat_utxo.json)
+if [ "${TXNS}" -eq "0" ]; then
+   echo -e "\n \033[0;31m NO UTxOs Found At ${collat_address} \033[0m \n";
+   exit;
+fi
+collat_utxo=$(jq -r 'keys[0]' ../tmp/collat_utxo.json)
 
+# script reference
+script_ref_utxo=$(${cli} transaction txid --tx-file ../tmp/swap-reference-utxo.signed )
+
+# time info
 slot=$(${cli} query tip --testnet-magic ${testnet_magic} | jq .slot)
 current_slot=$(($slot - 1))
 final_slot=$(($slot + 150))
 
-echo -e "\033[0;36m Building Tx \033[0m"
 FEE=$(${cli} transaction build \
     --babbage-era \
     --protocol-params-file ../tmp/protocol.json \
     --out-file ../tmp/tx.draft \
     --invalid-before ${current_slot} \
     --invalid-hereafter ${final_slot} \
-    --change-address ${buyer_address} \
-    --tx-in ${buyer_tx_in} \
-    --tx-in-collateral="${collat_utxo}#0" \
-    --tx-in ${script_tx_in}  \
+    --change-address ${seller_address} \
+    --tx-in ${seller_tx_in} \
+    --tx-in-collateral="${collat_utxo}" \
+    --tx-in ${auction_tx_in} \
+    --spending-tx-in-reference="${script_ref_utxo}#1" \
+    --spending-plutus-script-v2 \
+    --spending-reference-tx-in-inline-datum-present \
+    --spending-reference-tx-in-redeemer-file ../data/redeemers/offer-redeemer.json \
+    --tx-in ${bid_tx_in} \
     --spending-tx-in-reference="${script_ref_utxo}#1" \
     --spending-plutus-script-v2 \
     --spending-reference-tx-in-inline-datum-present \
     --spending-reference-tx-in-redeemer-file ../data/redeemers/complete-redeemer.json \
     --tx-out="${seller_address_out}" \
-    --tx-out="${sc_address_out}" \
-    --tx-out-inline-datum-file ../data/swappable/flatrate-swappable-datum.json  \
+    --tx-out="${script_address_out}" \
+    --tx-out-inline-datum-file ../data/swappable/buyer-swappable-datum.json \
     --required-signer-hash ${collat_pkh} \
+    --required-signer-hash ${seller_pkh} \
     --testnet-magic ${testnet_magic})
 
 IFS=':' read -ra VALUE <<< "${FEE}"
@@ -97,7 +160,7 @@ IFS=' ' read -ra FEE <<< "${VALUE[1]}"
 FEE=${FEE[1]}
 echo -e "\033[1;32m Fee: \033[0m" $FEE
 #
-# exit
+exit
 #
 echo -e "\033[0;36m Signing \033[0m"
 ${cli} transaction sign \
