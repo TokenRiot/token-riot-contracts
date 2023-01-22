efeda8b240d3ab122dd6e5cff3612f17d64aafdc
logicalmechanism <m.quinn.p@protonmail.com>
2023-01-22 11:34:40 -0800

    max amt max tkn stress test

--- contracts/scripts/swappable/07_transformSwappableUTxO.sh
+++ contracts/scripts/swappable/07_transformSwappableUTxO.sh
@@ -38,6 +38,22 @@ asset="1 4aa8f49a12e9eeaa45cf65c1db24e6bac563ed272145ec72b6c81fdb.5468697349734f
      + 1 c8e9790b2989b87ba8ef0a4b0fc38d2535bd616194af8b886f1f9422.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6730
      + 1 dc283b01a369c0b4ca7b19d23602230f5e5d15dd86a870a55e70bb8c.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6730"
 
+return_asset="1 3930c07047eb432bef46975ed52086d9250e21c19ad5c0c05ac35fb1.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6734
+        + 1 b98ceff806766d97d61a15deca05381c970d15d1d4da08869a3cdde8.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6734
+        + 1 961bbeddb4dabf8432c0dae1988d633043e64d56d5181cdde36265a9.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6734
+        + 1 08a1861472ae0b51049b21f8b58692bfb4bdec22d29169995bee74d6.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6734
+        + 1 e0fbf3c50833521e216506330f3a1c0805eda2559fadc3dbcd12a353.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6734
+        + 1 8df71632b3c9db50d19ec7a70457724188ef59be7a3b3bb0cabbba99.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6734
+        + 1 06f25e8d1376360d8ad84ffc1add8c7808a62216b98866435eaf9d5e.0acb50dcbce90a495aaab9e6b47483eef1582556541b97e2b45484e6fe37fcf5
+        + 1 43f43dd9db42399642f170cc410c73dd0d4e094f4b317c3ce9279069.007e08f756ee74d9d925df9c9881c717cf3c5fd510aedec0bf992c7a5a4a397a
+        + 1 5f4db4def248cafd08fda9e33e84454370160b12f269c882504dd5a2.16eb4ff3b84147a5cc8e301d8488508b3748be4fe948d4a2c25a2106513458f8
+        + 1 6effa18e41008cd0b13f3959a5a4af40b92ca936bb7669f40d3b1f81.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6730
+        + 1 9e70589a8dd23fd55f65198dc9e9a135751690d6bb3a63f7c28389ef.3773f3d1e8485f82e8bdf4660a5865c76d2672c9edc804af502529794c5f3c9f
+        + 1 ac74c28dcc6b051133f28ebb38cebfaf569f73a0b19ac8b9752c3796.012273743f3a872c685fcf6f7cd1ffa9157d8852b657b693178145aa83df13c3
+        + 1 cffbbfaef33f2e8f9ce99b0b7610a6403d5cb03d44d5977be66f46cb.015592033a2850503565abef1188040c41a3429234c83f6cb41042db0a51b404
+        + 1 d6784740a22d3bf55cbd99950035acaba1d7d71889f3f4d4866a9fa4.01c7e17dfef1086fd33930998b0d6e1fa4e8670362fc1e9369ff0580a3a64e7b
+        + 1 df8e2ddd300b54a606611a8f20be1aef06dc38d560e00b73e57de5a1.01207256bd3ea524e30189ec2f76e3d6e99ab8e2e5252ac84c4eaa455df46d91"
+
 
 # whats going back to the script
 script_min_utxo=$(${cli} transaction calculate-min-required-utxo \
@@ -47,13 +63,16 @@ script_min_utxo=$(${cli} transaction calculate-min-required-utxo \
     --tx-out="${script_address} + 5000000 + ${asset}" | tr -dc '0-9')
 
 # what is being returned to the seller
+receiver_address="addr_test1qrvnxkaylr4upwxfxctpxpcumj0fl6fdujdc72j8sgpraa9l4gu9er4t0w7udjvt2pqngddn6q4h8h3uv38p8p9cq82qav4lmp"
 return_min_utxo=$(${cli} transaction calculate-min-required-utxo \
     --babbage-era \
     --protocol-params-file ../tmp/protocol.json \
-    --tx-out="${script_address} + 5000000 + ${asset}" | tr -dc '0-9')
+    --tx-out="${receiver_address} + 5000000 + ${return_asset}" | tr -dc '0-9')
+
+
 
 script_address_out="${script_address} + ${script_min_utxo} + ${asset}"
-seller_address_out="${seller_address} + ${return_min_utxo}"
+seller_address_out="${receiver_address} + ${return_min_utxo} + ${return_asset}"
 echo "Transform OUTPUT: "${script_address_out}
 echo "Return OUTPUT: "${seller_address_out}
 #
@@ -141,7 +160,7 @@ IFS=' ' read -ra FEE <<< "${VALUE[1]}"
 FEE=${FEE[1]}
 echo -e "\033[1;32m Fee: \033[0m" $FEE
 #
-exit
+# exit
 #
 echo -e "\033[0;36m Signing \033[0m"
 ${cli} transaction sign \
